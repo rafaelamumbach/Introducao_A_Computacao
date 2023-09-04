869c72661ef2d0f90d9e3cb40ae20485b95213cb
Rafaela <rafaela.mumbach@acad.ufsm.br>
2023-09-04 14:23:51 -0300

    ok

---
+++ exerc1.py
@@ -0,0 +1,9 @@
+val1 = input("Informe um valor inteiro: ")
+val2 = input("Informe outro valor inteiro: ")
+
+mult = int (val1) * int (val2)
+print("A multiplicacao é: ", mult)
+
+## Desenvolva um programa em Python que solicite ao usuário digitar dois valores inteiros. Após a digitação, o computador
+## deverá executar a multiplicação destes números e mostrar o resultado na tela do computador. Exemplo: se o usuário digitar
+## os valores 11 e 13, o algoritmo deve realizar a multiplicação destes números e mostrar na tela o resultado (143).
